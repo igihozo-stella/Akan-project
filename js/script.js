e10c94c375d31cae51d7f1d5020de0ade10eb989
igihozo-stella <igihozo96@gmail.com>
2020-09-27 14:44:29 +0200

    add function myFunction in the javascript.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var year =document.getElementById('year').value;
+    var month =document.getElementById('month').value;
+    var date =document.getElementById('date').value;
+    
+
+}
