576ad63d26249907b4577cd1341758e1b52ac7d7
igihozo-stella <igihozo96@gmail.com>
2020-09-27 18:17:11 +0200

    Assign variables in the script file.

--- js/script.js
+++ js/script.js
@@ -2,10 +2,13 @@ function myFunction(){
     var year =document.getElementById('year').value;
     var month =document.getElementById('month').value;
     var date =document.getElementById('date').value;
-    var fullDate =year+"/"+month+"/"+date;
+    var fullDate=year+"/"+month+"/"+date;
+    var maleNames=['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+    var femaleNames=['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+    var gender =document.getElementById("gender").value;
 
     if((date <=0 || date>31) || (month <=0 || month >12)){
         alert("Invalid Data, Please Check your month or date");
-    }
+    } else if
 
 }
