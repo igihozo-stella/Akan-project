7ae369dbab0f58983a707f6dbf370a4c4f115a86
igihozo-stella <igihozo96@gmail.com>
2020-09-27 17:18:10 +0200

    add java script source and a link for google fonts to the html file.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ function myFunction(){
     var year =document.getElementById('year').value;
     var month =document.getElementById('month').value;
     var date =document.getElementById('date').value;
-    var 
+    var fullDate =year+"/"+month+"/"+date;
+
     if((date <=0 || date>31) || (month <=0 || month >12)){
         alert("Invalid Data, Please Check your month or date");
     }
