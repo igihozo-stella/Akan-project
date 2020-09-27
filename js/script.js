452ae3571b17c8d7c11ae28a4f85ea9d2296c8f2
igihozo-stella <igihozo96@gmail.com>
2020-09-27 14:52:15 +0200

    add alert in case for wrong month or day date.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ function myFunction(){
     var year =document.getElementById('year').value;
     var month =document.getElementById('month').value;
     var date =document.getElementById('date').value;
-    
+    var 
+    if((date <=0 || date>31) || (month <=0 || month >12)){
+        alert("Invalid Data, Please Check your month or date");
+    }
 
 }
